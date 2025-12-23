1e7351064980847a8dae3615f04fa1d7fad1b37e
DanriWeb <danri.web@gmail.com>
2025-12-23 17:52:27 +0500

    chore(deps): :tada: инициализация проекта React + TypeScript + Vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>MVP Flight Services</h1>
+      <p>Сервис дополнительных услуг для авиапассажиров</p>
+    </div>
+  );
+}
+
+export default App;
