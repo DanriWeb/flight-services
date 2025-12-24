241a68331849f426511385ec7df6e5c5fec642b2
DanriWeb <danri.web@gmail.com>
2025-12-24 12:34:17 +0500

    feat(features): :sparkles: обновить ServiceCard.tsx с новой логикой

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,3 +1,33 @@
-export default function ServiceCard() {
-  return <div></div>;
+import { useState } from "react";
+import "./ServiceCard.css";
+import type { Service } from "../types/service";
+import Button from "./Button";
+
+type ServiceCardProps = Pick<Service, "title" | "illustration" | "price">;
+
+export default function ServiceCard({
+  title,
+  price,
+  illustration,
+}: ServiceCardProps) {
+  const [isAdded, setIsAdded] = useState(false);
+
+  const handleAddClick = () => {
+    setIsAdded(!isAdded);
+  };
+
+  return (
+    <div className="service-card">
+      <div className="service-card__information-container">
+        <div className="service-card__illustration-container">
+          <img src={illustration} alt="" />
+        </div>
+        <div className="service-card__description-container">
+          <h3 className="service-card__service-title">{title}</h3>
+          <p className="service-card__service-price">${price}</p>
+        </div>
+      </div>
+      <Button size="medium" isAdded={isAdded} onClick={handleAddClick} />
+    </div>
+  );
 }
