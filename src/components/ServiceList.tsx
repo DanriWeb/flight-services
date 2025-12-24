c1cbd1685b956f31546e9dd191db94196cb955c8
DanriWeb <danri.web@gmail.com>
2025-12-24 12:33:29 +0500

    feat(features): :sparkles: обновить ServiceList.tsx с новой логикой

--- src/components/ServiceList.tsx
+++ src/components/ServiceList.tsx
@@ -1,3 +1,20 @@
+import "./ServiceList.css";
+import ServiceCard from "./ServiceCard";
+import { services } from "../data/services";
+
 export default function ServiceList() {
-  return <div></div>;
+  return (
+    <div className="service-list">
+      <div className="service-list__services-container">
+        {services.map((service) => (
+          <ServiceCard
+            key={service.id}
+            title={service.title}
+            price={service.price}
+            illustration={service.illustration}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
