42194f545d7485a7141d6d8228861da48042ed7f
DanriWeb <danri.web@gmail.com>
2025-12-24 14:28:44 +0500

    feat(features): :sparkles: доработать функционал Button.tsx
    - Добавлена галочка для состояния button--added

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
 import "./Button.css";
 import type { ButtonProps } from "../types/button";
+import CheckMarkIcon from "../assets/icons/Check Mark.svg?react";
 
 export default function Button({ size, isAdded, onClick }: ButtonProps) {
   return (
@@ -7,7 +8,14 @@ export default function Button({ size, isAdded, onClick }: ButtonProps) {
       className={`button button--${size} ${isAdded ? "button--added" : ""}`}
       onClick={onClick}
     >
-      {isAdded ? "Добавлено" : "Добавить"}
+      {isAdded ? (
+        <>
+          <CheckMarkIcon />
+          Добавлено
+        </>
+      ) : (
+        "Добавить"
+      )}
     </button>
   );
 }
