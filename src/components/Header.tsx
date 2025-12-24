3a6f2782d197352d0ebe38de9eb84debfd124431
DanriWeb <danri.web@gmail.com>
2025-12-24 11:37:21 +0500

    feat(ui): :lipstick: обновить header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
+import "./Header.css";
+
 export default function Header() {
   return (
-    <header>
-      <h1>MVP Flight Services</h1>
+    <header className="header">
+      <h1 className="header-title">MVP Flight Services</h1>
     </header>
   );
 }
